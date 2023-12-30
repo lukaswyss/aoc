1a7e4cc591c3a9d4bb0716285574c6fe5682719f
wyssl <lwyss4@online.gibz.ch>
2023-12-30 20:41:15 +0100

    day 9

--- 2023/Day9/part1.py
+++ 2023/Day9/part1.py
@@ -1,3 +1,12 @@
 import os
-   =  open(os.getcwd() + '/aoc/2023/Day9/text.txt', 'r', encoding='utf-8').read().split("\n\n")
+lines =  open(os.getcwd() + '/2023/Day9/text.txt', 'r', encoding='utf-8').read().split("\n")
+
+for line in lines:
+    numbers = line.split(" ")
+
+    for i ,number in numbers:
+        next = numbers[i + 1]
+        value =  next - number
+        print(value)
+
 
